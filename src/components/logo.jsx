13a7e2119ce28587604cb009401d04525c27191b
Rushel <ruselaq@gmail.com>
2025-06-01 20:23:34 +0800

    First Concept for Header

---
+++ src/components/logo.jsx
@@ -0,0 +1,24 @@
+export function Logo() {
+  return (
+    <svg
+     width="135"
+      height="40"
+      viewBox="0 0 135 60"
+      xmlns="http://www.w3.org/2000/svg"
+      className="text-foreground"
+    >
+      <text
+        x="50%"
+        y="50%"
+        fontFamily="Poppins"
+        fontWeight="bold"
+        fontSize="48"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fill="currentColor"
+      >
+        MORIME
+      </text>
+    </svg>
+  );
+}
