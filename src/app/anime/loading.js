da1915ab1ebdd6b75045c082fda77160e451dbfa
Rushel <ruselaq@gmail.com>
2025-06-11 01:44:49 +0800

    Revert "solve issue progressive loading system showing parent loading states during route resolution (maybe)"
    This reverts commit 50b52c94b19a5967cc9bef5aad19c62760afcd94.

--- src/app/anime/loading.js
+++ src/app/anime/loading.js
@@ -1,13 +1,5 @@
-"use client";
-import { usePathname } from "next/navigation";
 import { AnimeListSkeleton } from "@/components/skeleton/anime-list-skeleton";
 
 export default function Loading() {
-    const pathname = usePathname();
-
-    if (pathname !== '/anime') {
-        return null;
-    }
-
     return <AnimeListSkeleton showSearch={true} />;
 }
