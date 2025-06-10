50b52c94b19a5967cc9bef5aad19c62760afcd94
Rushel <ruselaq@gmail.com>
2025-06-11 01:36:25 +0800

    solve issue progressive loading system showing parent loading states during route resolution (maybe)

--- src/app/anime/loading.js
+++ src/app/anime/loading.js
@@ -1,5 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { AnimeListSkeleton } from "@/components/skeleton/anime-list-skeleton";
 
 export default function Loading() {
+    const pathname = usePathname();
+
+    if (pathname !== '/anime') {
+        return null;
+    }
+
     return <AnimeListSkeleton showSearch={true} />;
 }
