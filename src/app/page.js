2f124a29991e4fd71ce1b637fe3f036c0ac717a7
Rushel <ruselaq@gmail.com>
2025-06-26 01:06:04 +0800

    fix: solve (maybe) ratelimit issue

--- src/app/page.js
+++ src/app/page.js
@@ -5,27 +5,28 @@ import { getTopAnime, getAnimeGenresList } from "@/hooks/anime";
 import { getSeason } from "@/hooks/season";
 
 async function HomePageContent() {
-  const [upcomings, topAnimes, animes, genresList] = await Promise.all([
-    getSeason(3, { type: "seasons/upcoming", limit: 5 }),
-    getTopAnime(1, { limit: 20 }),
-    getSeason(1, { type: "seasons/now", limit: 20 }),
-    getAnimeGenresList(),
-  ]);
+    const upcomings = await getSeason(3, { type: "seasons/upcoming", limit: 6 });
 
-  return (
-    <HomePage
-      upcomings={upcomings.data}
-      topAnimes={topAnimes.data}
-      animes={animes.data}
-      genresList={genresList}
-    />
-  );
+    const [topAnimes, animes, genresList] = await Promise.all([
+        getTopAnime(1, { limit: 20 }),
+        getSeason(1, { type: "seasons/now", limit: 20 }),
+        getAnimeGenresList(),
+    ]);
+
+    return (
+        <HomePage
+            upcomings={upcomings.data}
+            topAnimes={topAnimes.data}
+            animes={animes.data}
+            genresList={genresList}
+        />
+    );
 }
 
 export default function Home() {
-  return (
-    <Suspense fallback={<HomePageSkeleton />}>
-      <HomePageContent />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<HomePageSkeleton />}>
+            <HomePageContent />
+        </Suspense>
+    );
 }
