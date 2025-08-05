765d44679addd9606456e88cd147081657252edd
Dawn-o <ruselaq@gmail.com>
2025-08-05 09:49:51 +0800

    fix: remove mistakenly added "Skeleton" to PageHeader class

--- src/components/headers/page-header.jsx
+++ src/components/headers/page-header.jsx
@@ -1,4 +1,4 @@
-export function PageHeaderSkeleton({ title, description }) {
+export function PageHeader({ title, description }) {
   return (
     <div className="text-center space-y-2 mb-8">
       <h1 className="text-2xl font-bold text-foreground">{title}</h1>
